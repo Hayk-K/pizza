308db518aeaf9947870576856dc982e5d2c27ed6
Hayk <khachatryan010101@gmail.com>
2022-03-02 17:28:07 +0400

    added component pizzaBlock

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import Home from "./pages/Home";
 import "./scss/app.scss";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([])
+
+ 
+
+  React.useEffect(() => {
+     fetch('http://localhost:3000/db.json')
+     .then(resp => resp.json())
+     .then( obj => setPizzas(obj.pizzas))
+  },[])
+
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home items={pizzas} />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
